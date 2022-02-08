5076fca631b28e3337de6f85da2d1572bb63e634
Chris Agbolade <yemluck@gmail.com>
2022-02-08 12:22:02 -0600

    Itinerary form created

--- src/components/ItineraryPage/ItineraryPage.jsx
+++ src/components/ItineraryPage/ItineraryPage.jsx
@@ -1,12 +1,17 @@
 import './ItineraryPage.css'
+import { useHistory } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import ItineraryForm from '../ItineraryForm/ItineraryForm';
 
 
 
 function Itinerary (){
-
+    const user = useSelector(store=>store.user)
     return( 
         <>
         <h1> This is the Itinerary page </h1>
+        <h3> Welcome {user.first_name} </h3>
+        <ItineraryForm />
         </>
     )
 }
