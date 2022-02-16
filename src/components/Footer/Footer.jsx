aae75e9ddf753778fa00e1026a618268faac429e
Chris Agbolade <yemluck@gmail.com>
2022-02-16 00:24:21 -0600

    styling changes to default registration page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ import './Footer.css';
 // or even care what the redux state is, so it doesn't need 'connect()'
 
 function Footer() {
-  return <footer>&copy; Chris International </footer>;
+  return <footer>&copy; Fly&Ship International </footer>;
 }
 
 export default Footer;
