5d26c53951ff843de31893b20c55cb8f010ad208
Chris Agbolade <yemluck@gmail.com>
2022-02-17 16:14:29 -0600

    about page

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -8,9 +8,26 @@ import React from 'react';
 function AboutPage() {
   return (
     <div className="container">
-      <div>
-        <p>This about page is for anyone to read!</p>
+      <center>
+      <div style={{maxWidth: 800}}>
+        <h3> About Fly&Ship</h3>
+        <p>Frequent flyers are aware of the rising cost of tickets. One factor that influences 
+          the price of tickets especially on international flights is the non-optional price of
+           luggage factored into the ticket prices:
+        </p>
+        <p>
+          People who ship internationally have been plagued with high cost of shipping and lack 
+          of security of products shipped. Courier services either charge high insurance to 
+          guarantee safe delivery of products or make customers liable for lost packages:
+        </p>
+        <p>
+          Fly&Ship Web App provides a solution to both problems described above. Frequent flyers 
+          could subsidize their cost of tickets by getting paid to transport packages at subsidized 
+          rates for international shippers. International shippers get the benefit of paying a fair 
+          prize and getting a guarantee of having their packages delivered promptly.
+        </p>
       </div>
+      </center>
     </div>
   );
 }
