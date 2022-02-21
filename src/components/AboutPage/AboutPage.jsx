09e7fb2726d1909e56712d7cad0372e9a9370fcf
Chris Agbolade <yemluck@gmail.com>
2022-02-20 19:10:03 -0600

    updated info page and upgraded readme.md

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -10,22 +10,47 @@ function AboutPage() {
     <div className="container">
       <center>
       <div style={{maxWidth: 800}}>
-        <h3> About Fly&Ship</h3>
-        <p>Frequent flyers are aware of the rising cost of tickets. One factor that influences 
-          the price of tickets especially on international flights is the non-optional price of
-           luggage factored into the ticket prices:
-        </p>
-        <p>
-          People who ship internationally have been plagued with high cost of shipping and lack 
-          of security of products shipped. Courier services either charge high insurance to 
-          guarantee safe delivery of products or make customers liable for lost packages:
-        </p>
-        <p>
-          Fly&Ship Web App provides a solution to both problems described above. Frequent flyers 
-          could subsidize their cost of tickets by getting paid to transport packages at subsidized 
-          rates for international shippers. International shippers get the benefit of paying a fair 
-          prize and getting a guarantee of having their packages delivered promptly.
-        </p>
+        <h2> About Fly&Ship</h2><br></br>
+        <h3>Technologies used</h3>
+          <a href="https://developer.mozilla.org/en-US/docs/Web/CSS">
+            <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg" 
+            height="40px" width="40px" /></a><a href="https://www.heroku.com/">
+              <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/heroku/heroku-original.svg" 
+              height="40px" width="40px" /></a><a href="https://developer.mozilla.org/en-US/docs/Web/HTML">
+                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg" 
+                height="40px" width="40px" /></a><a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">
+                  <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" 
+                  height="40px" width="40px" /></a><a href="https://jquery.com/">
+                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/jquery/jquery-original.svg"
+                     height="40px" width="40px" /></a><a href="https://material-ui.com/">
+                       <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/materialui/materialui-original.svg" 
+                       height="40px" width="40px" /></a><a href="https://nodejs.org/en/">
+                         <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original.svg" 
+                         height="40px" width="40px" /></a><a href="https://www.postgresql.org/">
+                           <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original.svg"
+                            height="40px" width="40px" /></a><a href="https://reactjs.org/">
+                              <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" 
+                              height="40px" width="40px" /></a><a href="https://redux.js.org/">
+                                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg"
+                                 height="40px" width="40px" />
+            </a>
+            <p><b> React.js, javascript, HTML, CSS, Passport.js, Multer for picture upload, Postgresql</b></p><br></br>
+            {/* <ul>
+              <li>React.js</li>
+              <li>javascript</li>
+              <li>Html & CSS</li>
+              <li>passport.js</li>
+              <li>multer for picture upload</li>
+              <li>PostGreSql</li>
+            </ul><br></br> */}
+            <h3> Challenges </h3>
+            <p>Sql filter and date data manipulation</p><br></br>
+            <h3> Future development to tackle</h3>
+            <p>Introduction of Map API for distance filtering</p>
+            <h3> Appreciation </h3>
+          <p>I acknowledge my Prime Woodall cohort, my instructor, my pod mates and everyone
+             who contributed to the success of this project</p>
+      
       </div>
       </center>
     </div>
--- src/components/InfoPage/InfoPage.jsx
+++ src/components/InfoPage/InfoPage.jsx
@@ -8,7 +8,26 @@ import React from 'react';
 function InfoPage() {
   return (
     <div className="container">
-      <p>Info Page</p>
+      <center>
+        <div style={{ maxWidth: 800 }}>
+          <h3><u> Information about Fly&Ship </u></h3>
+          <p>Frequent flyers are aware of the rising cost of tickets. One factor that influences
+            the price of tickets especially on international flights is the non-optional price of
+            luggage factored into the ticket prices:
+          </p>
+          <p>
+            People who ship internationally have been plagued with high cost of shipping and lack
+            of security of products shipped. Courier services either charge high insurance to
+            guarantee safe delivery of products or make customers liable for lost packages:
+          </p>
+          <p>
+            Fly&Ship Web App provides a solution to both problems described above. Frequent flyers
+            could subsidize their cost of tickets by getting paid to transport packages at subsidized
+            rates for international shippers. International shippers get the benefit of paying a fair
+            prize and getting a guarantee of having their packages delivered promptly.
+          </p>
+        </div>
+      </center>
     </div>
   );
 }
